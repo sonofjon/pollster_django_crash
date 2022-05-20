0e034f63ffc1333c10bfe9e691697517b6af4840
Andreas Jonsson <ajdev8@gmail.com>
2022-05-20 14:55:07 +0200

    Translations

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -46,7 +46,7 @@ def vote(request, question_id):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice.",
+            'error_message': "Du har inte valt något alternativ.",
         })
     else:
         selected_choice.votes += 1

c95e434bfea94b1ee7eb939fd39d6f1e18a5353f
Andreas Jonsson <ajdev8@gmail.com>
2022-05-27 11:57:42 +0200

    Modify max character length for questions and choices

--- pollster/polls/models.py
+++ pollster/polls/models.py
@@ -11,7 +11,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    answer_text = models.CharField(max_length=500)
+    answer_text = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.answer_text
@@ -19,7 +19,7 @@ class Answer(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
 
     def __str__(self):

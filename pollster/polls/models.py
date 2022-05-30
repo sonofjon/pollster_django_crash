a1f285426219222f728da4e69ca0ebc703889b44
Andreas Jonsson <ajdev8@gmail.com>
2022-05-30 23:32:19 +0200

    Remove question, answer and publication date from Question model

--- pollster/polls/models.py
+++ pollster/polls/models.py
@@ -2,13 +2,7 @@ from django.db import models
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    answer_text = models.CharField(max_length=1000, default=None)
     image = models.ImageField(upload_to="images/", default=None)
-    pub_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.question_text
 
 
 class Choice(models.Model):

c95ffeeaf294e0d297e8d696b6633ad283995bfc
Andreas Jonsson <ajdev8@gmail.com>
2023-03-24 21:31:04 +0100

    Add wip from brain10-windows

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.forms.models import BaseInlineFormSet
 
 from .models import Question, Choice
 
@@ -7,9 +8,18 @@ admin.site.site_title = "Pollster Admin Area"
 admin.site.index_title = "Welcome to the Pollster admin area"
 
 
+class ChoiceInlineFormSet(BaseInlineFormSet):
+    def __init__(self, *args, **kwargs):
+        kwargs['initial'] = [
+            {'choice_text': 'JA'}, {'choice_text': 'NEJ'}
+        ]
+        super(ChoiceInlineFormSet, self).__init__(*args, **kwargs)
+
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     max_num = 2
+    formset = ChoiceInlineFormSet
 
 
 class QuestionAdmin(admin.ModelAdmin):
